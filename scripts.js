f7c3e4fd44050ea5d76dab7e8643c28c19ef9b8a
Alexandr Gavrilenko <acoolfreak@gmail.com>
2022-03-27 00:57:44 +0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let i=1
+while (i<101){
+    if (i%3==0 && i%5==0){
+        console.log("FizzBuzz");
+        i++
+    }
+    else if (i%3==0) {console.log('Fizz'); i++}
+    else if (i%5==0) {console.log('Buzz'); i++}
+    else {console.log(i); i++}
+}
